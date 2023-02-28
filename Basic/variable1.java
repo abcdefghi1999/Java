2b849b9608208af066782d6484d4a5d3a54c7911
abcdefghi1999 <117515772+abcdefghi1999@users.noreply.github.com>
2023-02-28 13:13:13 +0530

    Create variable1.java

---
+++ Basic/variable1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		Scanner sc = new Scanner(System.in);
+		int var = 10;
+		System.out.print("My num is " + var);
+    }
+}
